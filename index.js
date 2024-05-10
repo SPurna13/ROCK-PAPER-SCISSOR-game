d06f7aeb90a3a45240343c980ecdae6d9197ef62
S.PURNA JYOTHI <138035435+SPurna13@users.noreply.github.com>
2024-05-10 18:04:52 +0530

    index.js

--- index.js
+++ index.js
@@ -1,80 +1,72 @@
 var arr=["ROCK","PAPER","SCISSOR"];
-var cnt=0,cnt1=0,num,count=0;
+let obj={
+    cnt:0,cnt1:0,count:0,
+}
 function check()
-{count++;
-    var num=Math.floor(Math.random()*3);
-    if(arr[num]=="PAPER")
-    {
-        cnt1+=1;
-    }
-    if(arr[num]=="SCISSOR"){
-        cnt+=1
-    }
-    fun(cnt,cnt1,count,arr[num],"ROCK");
+{
+    obj.count+=1;
+    let num=Math.floor(Math.random()*3);
+    fun(arr[num],"ROCK");
 }
 function check1()
-{count++;
-    var num=Math.floor(Math.random()*3);
-    if (arr[num]=="ROCK")
-    {
-        cnt+=1;
-    }
-    if (arr[num]=="SCISSOR"){
-        cnt1+=1
-    }
-    fun(cnt,cnt1,count,arr[num],"PAPER");
+{
+    obj.count+=1;
+    let num=Math.floor(Math.random()*3);
+    fun(arr[num],"PAPER");
 }
 function check2()
-{count++;
-    var num=Math.floor(Math.random()*3);
-    if (arr[num]=="ROCK")
-    {
-        cnt1+=1
-    }
-    if(arr[num]=="PAPER")
-    {
-        cnt1+=1;
-    }
-    fun(cnt,cnt1,count,arr[num],"SCISSOR");
-}
-function fun(cnt,cnt1,count,a,b)
 {
-    if(count<=5)
-    {
-        var a=document.getElementById("four").innerHTML="You Chose: "+a+" && Computer Chose: "+b;
-        var d=document.getElementById("first").innerHTML="YOUR SCORE:"+cnt;
-        var e=document.getElementById("second").innerHTML="COMPUTER'S SCORE:"+cnt1;
-    }
-    if (count==5)
-        {
-        fn(cnt,cnt1);
-        }
-    else if(count>5){
-        var g=document.getElementById("four").innerHTML="";
-        var d=document.getElementById("first").innerHTML="";
-        var e=document.getElementById("second").innerHTML="";
-        var f=document.getElementById("third").innerHTML="RESTART THE GAME↻";
-        }
+    obj.count+=1;
+    let num=Math.floor(Math.random()*3);
+    fun(arr[num],"SCISSOR");
 }
+function fun(a,b)
+{
+    if ((a=="ROCK"&&b=="PAPER")||(a=="PAPER"&&b=="SCISSOR")||(a=="SCISSOR"&&b=="ROCK"))
+        {
+            obj.cnt1+=1;
+        }
+    if ((a=="ROCK" && b=="SCISSOR")||(a=="SCISSOR"&&b=="PAPER")||(a=="PAPER"&&b=="ROCK"))
+        {
+            obj.cnt+=1;
+        }
+    if(obj.count<=5)
+        {
+            let f=document.getElementById("four").innerHTML="You Chose: "+a +" && Computer Chose: "+b;
+            let d=document.getElementById("first").innerHTML="YOUR SCORE:"+obj.cnt;
+            let e=document.getElementById("second").innerHTML="COMPUTER'S SCORE:"+obj.cnt1;
+        }
+    if(obj.count==5)
+        {
+            fn(obj.cnt,obj.cnt1);
+        }
+    else if(obj.count>5)
+        {
+            let g=document.getElementById("four").innerHTML="";
+            let d=document.getElementById("first").innerHTML="";
+            let e=document.getElementById("second").innerHTML="";
+            let f=document.getElementById("third").innerHTML="RESTART THE GAME↻";
+        }
+}  
 function fn(cnt,cnt1)
 {
     if(cnt>cnt1)
     {
-        var e=document.getElementById("third").innerHTML="YOU WON &#127881";
+        let e=document.getElementById("third").innerHTML="YOU WIN &#127881";
     }
     else if(cnt1>cnt)
     {
-        var e=document.getElementById("third").innerHTML="COMPUTER WINS &#128187";
+        let e=document.getElementById("third").innerHTML="COMPUTER WINS &#128187";
     }
     else{
-        var e=document.getElementById("third").innerHTML="IT'S TIE";
+        let e=document.getElementById("third").innerHTML="IT'S TIE";
     }
 }
 function restart()
 {
-    count=0,cnt=0,cnt1=0;
-    var g=document.getElementById("four").innerHTML="";
-    var f=document.getElementById("third").innerHTML="";
-    var d=document.getElementById("first").innerHTML="";
-    var e=document.getElementById("second").innerHTML="";
-}
+    obj.count=0,obj.cnt=0,obj.cnt1=0;
+    let g=document.getElementById("four").innerHTML="";
+    let f=document.getElementById("third").innerHTML="";
+    let d=document.getElementById("first").innerHTML="";
+    let e=document.getElementById("second").innerHTML="";
+}    
